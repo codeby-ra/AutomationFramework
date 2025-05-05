180f15efdd3957468c1fb40b9697c5fd85b6a529
rene-alano <renealanoxd@gmail.com>
2025-05-05 20:46:28 +0800

    Creating the base and utilities class

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -1,4 +1,31 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class DriverFactory {
+
+    private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+
+    public static WebDriver initDriver(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            tlDriver.set(new ChromeDriver());
+        }
+        getDriver().manage().window().maximize();
+        return getDriver();
+    }
+
+
+    public static WebDriver getDriver() {
+        return tlDriver.get();
+    }
+
+    public static void quitDriver() {
+        getDriver().quit();
+        tlDriver.remove();
+    }
+
+
 }
---
+++ src/main/java/utils/WaitUtils.java
@@ -0,0 +1,36 @@
+package utils;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtils {
+
+    private WebDriver driver;
+
+    public WaitUtils(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    public WebElement waitForVisibility(WebElement element, int timeoutSeconds) {
+        return new WebDriverWait(driver, Duration.ofSeconds(timeoutSeconds)).until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public WebElement waitForClickability(WebElement element, int timeoutSeconds) {
+        return new WebDriverWait(driver, Duration.ofSeconds(timeoutSeconds)).until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public boolean isDisplayed(WebElement element){
+        try {
+            return element.isDisplayed();
+        } catch (NoSuchElementException | StaleElementReferenceException e){
+            return false;
+        }
+    }
+}
