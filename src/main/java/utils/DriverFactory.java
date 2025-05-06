ee7522f3b2a394b18ed3c2657f2c6822969a6f87
rene-alano <renealanoxd@gmail.com>
2025-05-06 21:31:19 +0800

    Enhancing the DriverFactory.java

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -1,30 +1,60 @@
 package utils;
 
+import config.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 
 public class DriverFactory {
 
-    private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
 
     public static WebDriver initDriver(String browser) {
-        if (browser.equalsIgnoreCase("chrome")) {
-            tlDriver.set(new ChromeDriver());
+        browser = browser.toLowerCase().trim();
+
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--start-maximized");
+                driver.set(new ChromeDriver(chromeOptions));
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--start-maximized");
+                driver.set(new FirefoxDriver(firefoxOptions));
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--start-maximized");
+                driver.set(new EdgeDriver(edgeOptions));
+                break;
+            default:
+                throw new RuntimeException("❗ Browser not supported: " + browser);
         }
-        getDriver().manage().window().maximize();
-        return getDriver();
+        driver.get().get(ConfigReader.get("baseUrl"));
+        return driver.get();
     }
 
 
     public static WebDriver getDriver() {
-        return tlDriver.get();
+        return driver.get();
     }
 
     public static void quitDriver() {
-        getDriver().quit();
-        tlDriver.remove();
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
     }
 
 
