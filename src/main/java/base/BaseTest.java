a53a779598b68a185d67a033173024d1a7e1fbc3
rene-alano <renealanoxd@gmail.com>
2025-05-06 21:30:56 +0800

    Enhancing the BaseTest.java

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,21 +1,63 @@
 package base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import config.ConfigReader;
+import listeners.TestListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 import utils.DriverFactory;
 
+@Listeners(TestListener.class)
 public class BaseTest {
 
-    protected WebDriver driver;
+    protected static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    protected static ExtentReports extent;
+    protected ExtentTest test;
+    protected static Logger logger;
+
+
+    @BeforeSuite
+    public void setupSuite() {
+        ConfigReader.loadProperties();
+//        extent = ReportManager.createInstance();
+        logger = LogManager.getLogger(BaseTest.class);
+        logger.info("===== Test Suite Started =====");
+    }
 
     @BeforeMethod
-    public void setUp() {
-        driver = DriverFactory.initDriver("chrome");
+    @Parameters("browser")
+    public void setUp(@Optional("chrome") String browser, java.lang.reflect.Method method) {
+        WebDriver webDriver = DriverFactory.initDriver(browser);
+        driver.set(webDriver);
+
+        test = extent.createTest(method.getName());
+        logger.info("Starting test: " + method.getName());
     }
 
     @AfterMethod
-    public void tearDown() {
-        DriverFactory.quitDriver();
+    public void tearDown(org.testng.ITestResult result) {
+        if (result.getStatus() == org.testng.ITestResult.FAILURE) {
+            logger.error("Test Failed: " + result.getName());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            logger.info("Test Passed: " + result.getName());
+        }
+
+        driver.get().quit();
+        logger.info("Browser closed.");
+    }
+
+    @AfterSuite
+    public void tearDownSuite() {
+        extent.flush();
+        logger.info("===== Test Suite Finished =====");
+    }
+
+
+    public WebDriver getDriver() {
+        return driver.get();
     }
 }
